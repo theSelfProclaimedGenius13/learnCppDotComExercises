f3f815109e0d452c84f37aad5907c739426c9daf
theSelfProclaimedGenius13 <theselfproclaimedgenius13@gmail.com>
2025-10-09 22:32:24 +0530

    completion of chapter 5

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "chap_4_x_q3.h"
+#include "chap_5_7_q1.h"
 #include <iostream>
 
 int main() {
 	//std::cout << "Hello, World!" << std::endl;
-	result_message_chap_4_x_q3();
+	result_message_chap_5_7_q1();
 	return 0;
 }	
