621bfe1da3755e9906ce2de8477262006fde4aa9
theSelfProclaimedGenius13 <theselfproclaimedgenius13@gmail.com>
2025-10-10 20:41:04 +0530

    Completion Of Chapter 5.X and all practice exercises.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "chap_5_7_q1.h"
+#include "chap_5_x_q4.h"
 #include <iostream>
 
 int main() {
 	//std::cout << "Hello, World!" << std::endl;
-	result_message_chap_5_7_q1();
+	result_message_chap_5_x_q4();
 	return 0;
 }	
