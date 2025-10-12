07ffba864db9ff5a8ff75542547f4845eed39e90
theSelfProclaimedGenius13 <theselfproclaimedgenius13@gmail.com>
2025-10-12 16:37:00 +0530

    Completion of Chapter 6.8.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include "chap_6_3_q2.h"
+#include "chap_6_8_q2.h"
 #include <iostream>
-
 int main() {
-	//std::cout << "Hello, World!" << std::endl;
-	result_message_chap_6_3_q2();
+	result_message_chap_6_8_q2();
 	return 0;
 }	
